cd8c72f6f5f761899036f8f2363c0257d8b96398
martinkieliszek_nbnco <martinkieliszek@nbnco.com.au>
2026-01-02 17:15:43 +1100

    Initial Commit

---
+++ StandFit/StandFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  StandFitApp.swift
+//  StandFit
+//
+//  Created by Marty Kieliszek on 2/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct StandFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
